73519ff4676d111a1d2c397ef231cb7a40c21e0c
AkashJana18 <akashjana663@gmail.com>
2023-08-20 22:33:15 +0530

    Added Carousel using Swiper and Added Accordion in the Value Section

--- src/utils/accordion.jsx
+++ src/utils/accordion.jsx
@@ -5,19 +5,19 @@ const data = [
     icon: <HiShieldCheck/>,
     heading: "Best interest rates on the market",
     detail:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Unlock Your Dream Home: Discover the Best Interest Rates on the Market with Our Real Estate Platform, ensuring you make the most of your budget while realizing your homeownership dreams.",
   },
   {
     icon: <MdCancel />,
     heading: "Prevent unstable prices",
     detail:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Steadfast Stability: Navigate Unpredictability and Prevent Unstable Prices through Our Real Estate Listings, offering you a reliable path to property ownership and investment.",
   },
   {
     icon: <MdAnalytics />,
     heading: "Best price on the market",
     detail:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Your Perfect Investment: Secure the Best Price on the Market for Your Ideal Property with Our Real Estate Website, where your real estate aspirations meet unparalleled value.",
   },
 ];
 export default data;
